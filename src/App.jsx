ef941303207eb83b3bf215e1956ef0824aa3a94f
Ferdous <hassanferdous101@gmail.com>
2024-01-19 20:42:38 +0600

    fixed transparent image

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,77 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useRef, useState } from "react";
-import { Image, Layer, Stage } from "react-konva";
+import React, {useEffect, useRef, useState} from "react";
+import {useLayoutEffect} from "react";
+import {Image, Layer, Stage} from "react-konva";
 import useImage from "use-image";
 
 function URLImage(props) {
-  const imageRef = useRef(null);
-  const [image, setImage] = useState(null);
+	const imageRef = useRef(null);
+	const [image, setImage] = useState(null);
 
-  useEffect(() => {
-    loadImage();
-    return () => {
-      if (imageRef.current) {
-        imageRef.current.removeEventListener("load", handleLoad);
-      }
-    };
-  }, []);
+	useEffect(() => {
+		loadImage();
+		return () => {
+			if (imageRef.current) {
+				imageRef.current.removeEventListener("load", handleLoad);
+			}
+		};
+	}, []);
 
-  useEffect(() => {
-    loadImage();
-  }, [props.src]);
+	useEffect(() => {
+		loadImage();
+	}, [props.src]);
 
-  function handleLoad() {
-    setImage(imageRef.current);
-  }
+	function handleLoad() {
+		setImage(imageRef.current);
+	}
 
-  function loadImage() {
-    const img = new window.Image();
-    img.src = props.src;
-    img.crossOrigin = "Anonymous";
-    imageRef.current = img;
-    imageRef.current.addEventListener("load", handleLoad);
-  }
+	function loadImage() {
+		const img = new window.Image();
+		img.src = props.src;
+		img.crossOrigin = "Anonymous";
+		imageRef.current = img;
+		imageRef.current.addEventListener("load", handleLoad);
+	}
 
-  // return <image x="{props.x}" y="{props.y}" image="{image}" />;
+	// return <image x="{props.x}" y="{props.y}" image="{image}" />;
 }
-const MaskImage = ({ src }) => {
-  const [image] = useImage(src);
-  return (
-    <Image
-      filters={[{ x: 0, y: 0, width: 100, height: 100, image }]}
-      onClick={(e) => {
-        console.log("click", e);
-      }}
-      image={image}
-      draggable
-    />
-  );
+const MaskImage = ({src}) => {
+	const [image] = useImage(src);
+	const ref = useRef();
+	useLayoutEffect(() => {
+		ref.current.cache();
+		ref.current.drawHitFromCache();
+		console.log(ref.current.width());
+	}, [image]);
+	return (
+		<Image
+			onClick={(e) => {
+				console.log("click", e);
+			}}
+			width={window.innerWidth}
+			height={window.innerHeight}
+			image={image}
+			draggable
+			ref={ref}
+		/>
+	);
 };
 const App = () => {
-  const stageRef = useRef(null);
+	const stageRef = useRef(null);
 
-  useEffect(() => {
-    console.log(stageRef.current.canvas.getContext());
-  }, []);
-  return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer ref={stageRef} width={window.innerWidth} height={"auto"}>
-        <MaskImage src="/images/original.jpg" />
-        <MaskImage src={"/images/mask_1.png"} />
-        <MaskImage src={"/images/mask_2.png"} />
-        <MaskImage src={"/images/mask_3.png"} />
-      </Layer>
-    </Stage>
-  );
+	// useEffect(() => {
+	// 	console.log(stageRef.current.canvas.getContext());
+	// }, []);
+	return (
+		<Stage width={window.innerWidth} height={window.innerHeight}>
+			<Layer ref={stageRef}>
+				<MaskImage src="/images/original.jpg" />
+				<MaskImage src={"/images/mask_1.png"} />
+				<MaskImage src={"/images/mask_2.png"} />
+				<MaskImage src={"/images/mask_3.png"} />
+			</Layer>
+		</Stage>
+	);
 };
 
 export default App;
